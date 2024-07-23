6c4e6aafe239250fadf83bda08a3ad6dcfd84b68
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-07-23 13:01:12 +0200

    ➕Reusable button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,16 @@
 import {Link} from "react-router-dom";
 import { FaCircleArrowRight as Arrow } from "react-icons/fa6";
 
-const Button = ({title, hasIcon}) => {
+const Button = ({children, hasIcon, className}) => {
     return (
-        <Link to={""} className="inline-block rounded-[16px] px-[39px] py-[28px] items-center bg-yellow">
+        <Link to={""} className={`inline-block rounded-[16px] px-[39px] py-[20px] items-center ${className ? className : 'bg-yellow'}`}>
             <div className="flex items-center justify-center gap-[10px]">
-                <span className="font-bold text-darkGreen">
-                {title}
+                <span className={`font-bold ${className?`text-white` : `text-darkGreen`}`}>
+                {children}
             </span>
                 {
                     hasIcon ? (
-                        <span className={"text-darkGreen"}>
+                        <span className={className? `text-[#335B6B]` : "text-darkGreen"}>
                             <Arrow />
                         </span>
                     ) : ""
