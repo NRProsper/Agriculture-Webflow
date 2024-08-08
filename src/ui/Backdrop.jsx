ac330df70ca3376af32f07d199110dbed0e6ac5a
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-08 21:18:05 +0200

    Finished my Dialog

--- src/ui/Backdrop.jsx
+++ src/ui/Backdrop.jsx
@@ -4,7 +4,7 @@ import {motion} from "framer-motion";
 const Backdrop = ({children, onClick}) => {
     return(
         <motion.div
-            className="absolute top-0 z-[999] left-0 w-full h-full bg-black/50 flex items-center justify-center"
+            className="absolute top-0 z-[999] left-0 w-full h-screen bg-black/70 flex items-center justify-center"
             onClick={onClick}
             initial={{opacity: 0}}
             animate={{opacity: 1}}
